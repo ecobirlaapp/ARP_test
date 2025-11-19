cb53b8b481fad1242caff56a42ca4e41f40b7920
ecobirlaapp <ecobirlaapp@gmail.com>
2025-11-19 15:51:46 +0530

    Update state.js

--- state.js
+++ state.js
@@ -1,13 +1,25 @@
+export const CLOUDINARY_CLOUD_NAME = 'dnia8lb2q';
+export const CLOUDINARY_UPLOAD_PRESET = 'EcoBirla_avatars';
+export const CLOUDINARY_API_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/auto/upload`;
+
+export const TICK_IMAGES = {
+    blue: 'https://i.ibb.co/kgJpMCHr/blue.png',
+    silver: 'https://i.ibb.co/gLJLF9Z2/silver.png',
+    gold: 'https://i.ibb.co/Q2C7MrM/gold.png',
+    black: 'https://i.ibb.co/zVNSNzrK/black.png',
+    green: 'https://i.ibb.co/SXGL4Nq0/green.png'
+};
+
 export let state = {
     currentUser: null, 
     userAuth: null,    
     checkInReward: 10,
     leaderboard: [],
+    departmentLeaderboard: [],
     stores: [],
     products: [],      
     history: [],
     dailyChallenges: [],
-    activeQuiz: null,  
     events: [],
     userRewards: [],   
     levels: [
